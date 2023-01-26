b29f9da6d954cca42ef65d25833530ca4eb16ae0
Joshua Verne <joshuaverne@gmail.com>
2023-01-26 15:07:22 -0500

    typo

--- mysite/gallery/admin.py
+++ mysite/gallery/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from .models import GalleryPiece, Exhibition
 
 # Register your models here.
-admin.site.register(GalleryPiece);
-admin.site.register(Exhibition);
+admin.site.register(GalleryPiece)
+admin.site.register(Exhibition)
