7081bdadf1d9e6e651f746d9ce2269eef10a5890
Joshua Verne <joshuaverne@gmail.com>
2023-01-26 16:18:53 -0500

    gallery views

--- mysite/gallery/urls.py
+++ mysite/gallery/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /gallery/
     path('', views.index, name='index'),
+    # ex: /gallery/exhib/5
+    path('exhib/<int:exhibition_id>', views.exhibition_detail, name='exhibition_detail'),
+    # ex: /gallery/piece/5
+    path('piece/<int:piece_id>', views.piece_detail, name='piece_detail')
+
 ]
--- mysite/gallery/views.py
+++ mysite/gallery/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import GalleryPiece, Exhibition
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the gallery index.")
+    exhibition_list = Exhibition.objects.all()
+    pieces_list = GalleryPiece.objects.all()
+    output = ', '.join([e.name for e in exhibition_list])
+    output += ', '.join(p.title for p in pieces_list)
+    return HttpResponse(output)
+
+def exhibition_detail(request, exhibition_id):
+    return HttpResponse("You're looking at exhibition %s." % exhibition_id)
+
+def piece_detail(request, piece_id):
+    return HttpResponse("You're looking at piece %s." % piece_id)
