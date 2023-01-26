29701d8c5acc95de97d31d58cc140fcbaf6d23ca
Joshua Verne <joshuaverne@gmail.com>
2023-01-26 15:06:15 -0500

    registered new models with admin

--- mysite/gallery/admin.py
+++ mysite/gallery/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import GalleryPiece, Exhibition
+
 # Register your models here.
+admin.site.register(GalleryPiece);
+admin.site.register(Exhibition);
