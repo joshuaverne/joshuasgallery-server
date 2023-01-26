f6e91a78e7998d2ba48d906af12e428b8457b3f6
Joshua Verne <joshuaverne@gmail.com>
2023-01-26 14:46:51 -0500

    created models GalleryPiece and Exhibition

--- mysite/gallery/models.py
+++ mysite/gallery/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Exhibition(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class GalleryPiece(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published to gallery')
+    galleries = models.ManyToManyField(Exhibition)
+
+    def __str__(self):
+        return self.title
+
