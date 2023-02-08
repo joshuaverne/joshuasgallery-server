c7fbb02ea3ae7a2da135bc28a847f308de232474
Joshua Verne <joshuaverne@gmail.com>
2023-02-08 11:09:12 -0500

    removed crispy forms module, added login test

--- mysite/test/tests.py
+++ mysite/test/tests.py
@@ -7,19 +7,38 @@ from selenium.webdriver.common.by import By
 class LoginTest(LiveServerTestCase):
 
     def testLogin(self):
-        failed_login_msg = 'Please enter the correct username and password for a staff account.'
+        successful_login_msg = 'You\'re at the homepage!'
 
         selenium = webdriver.Chrome()
 
-        selenium.get('http://localhost:8000/admin')
+        selenium.get('http://localhost:8000/login')
 
         username_field = selenium.find_element(By.ID, 'id_username')
         password_field = selenium.find_element(By.ID, 'id_password')
 
-        submit_btn = selenium.find_element(By.XPATH, "//input[@type='submit']")
+        submit_btn = selenium.find_element(By.XPATH, "//button[@type='submit']")
 
-        username_field.send_keys('notRealUsername')
-        password_field.send_keys('notRealPassword')
+        username_field.send_keys('testuser')
+        password_field.send_keys('mypw1234')
+
+        submit_btn.send_keys(Keys.RETURN)
+
+        assert successful_login_msg in selenium.page_source
+
+    def testFailedLogin(self):
+        failed_login_msg = 'Invalid username or password.'
+
+        selenium = webdriver.Chrome()
+
+        selenium.get('http://localhost:8000/login')
+
+        username_field = selenium.find_element(By.ID, 'id_username')
+        password_field = selenium.find_element(By.ID, 'id_password')
+
+        submit_btn = selenium.find_element(By.XPATH, "//button[@type='submit']")
+
+        username_field.send_keys('nonexistantuser')
+        password_field.send_keys('notarealpassword')
 
         submit_btn.send_keys(Keys.RETURN)
 
