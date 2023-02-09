96c042b54747c3bb702d44c4a16b52c7cdac2a2d
Joshua Verne <joshuaverne@gmail.com>
2023-02-09 16:31:44 -0500

    added gallery dashboard test, added element IDs to header template for testing

--- mysite/test/tests.py
+++ mysite/test/tests.py
@@ -2,44 +2,92 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import ElementNotInteractableException
 
 
 class LoginTest(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = webdriver.Chrome()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
 
     def testLogin(self):
         successful_login_msg = 'You\'re at the homepage!'
 
-        selenium = webdriver.Chrome()
+        self.selenium.get('http://localhost:8000/login')
 
-        selenium.get('http://localhost:8000/login')
+        username_field = self.selenium.find_element(By.ID, 'id_username')
+        password_field = self.selenium.find_element(By.ID, 'id_password')
 
-        username_field = selenium.find_element(By.ID, 'id_username')
-        password_field = selenium.find_element(By.ID, 'id_password')
-
-        submit_btn = selenium.find_element(By.XPATH, "//button[@type='submit']")
+        submit_btn = self.selenium.find_element(By.XPATH, "//button[@type='submit']")
 
         username_field.send_keys('testuser')
         password_field.send_keys('mypw1234')
 
         submit_btn.send_keys(Keys.RETURN)
 
-        assert successful_login_msg in selenium.page_source
+        assert successful_login_msg in self.selenium.page_source
 
     def testFailedLogin(self):
         failed_login_msg = 'Invalid username or password.'
 
-        selenium = webdriver.Chrome()
+        self.selenium.get('http://localhost:8000/login')
 
-        selenium.get('http://localhost:8000/login')
+        username_field = self.selenium.find_element(By.ID, 'id_username')
+        password_field = self.selenium.find_element(By.ID, 'id_password')
 
-        username_field = selenium.find_element(By.ID, 'id_username')
-        password_field = selenium.find_element(By.ID, 'id_password')
-
-        submit_btn = selenium.find_element(By.XPATH, "//button[@type='submit']")
+        submit_btn = self.selenium.find_element(By.XPATH, "//button[@type='submit']")
 
         username_field.send_keys('nonexistantuser')
         password_field.send_keys('notarealpassword')
 
         submit_btn.send_keys(Keys.RETURN)
 
-        assert failed_login_msg in selenium.page_source
+        assert failed_login_msg in self.selenium.page_source
+
+
+class NavTest(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = webdriver.Chrome()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def login(self):
+        self.selenium.get('http://localhost:8000/login')
+
+        username_field = self.selenium.find_element(By.ID, 'id_username')
+        password_field = self.selenium.find_element(By.ID, 'id_password')
+
+        submit_btn = self.selenium.find_element(By.XPATH, "//button[@type='submit']")
+
+        username_field.send_keys('testuser')
+        password_field.send_keys('mypw1234')
+
+        submit_btn.send_keys(Keys.RETURN)
+
+    def testGalleryDash(self):
+        gallery_text = "Welcome to the gallery dashboard!"
+
+        self.login()
+        nav_btn = self.selenium.find_element(By.ID, "navbar_toggle_button")
+
+        if nav_btn.is_displayed():
+            nav_btn.click()
+
+        self.selenium.find_element(By.ID, "nav_link_my_gallery").click()
+
+        assert gallery_text in self.selenium.page_source
+
+
