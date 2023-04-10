4f1ca7c896bd379ce0cab74508e7a69d00c6c4c5
Joshua Verne <joshuaverne@gmail.com>
2023-04-10 13:20:44 -0400

    removed unused forms.py

--- mysite/gallery/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class NewGalleryPieceForm(forms.Form):
-    title = forms.CharField(label='Title', max_length=500, required=False)
-    description = forms.CharField(label='Description', max_length=1000, required=False)
