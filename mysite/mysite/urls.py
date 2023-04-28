4a95c07cbf0c7c5d6a74222031a8a1f7f85b5380
Joshua Verne <joshuaverne@gmail.com>
2023-04-28 15:07:15 -0400

    removed erroneous URL conf for serving images

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -28,6 +28,3 @@ urlpatterns = [
     path('login/', views.login_request, name="login"),
     path('logout/', views.logout_request, name="logout")
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
