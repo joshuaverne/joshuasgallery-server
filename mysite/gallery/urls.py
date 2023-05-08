03035f4f1a6289e2b37ac0396ce2a7c4c69109ff
Joshua Verne <joshuaverne@gmail.com>
2023-05-08 15:20:33 -0400

    fixed some links without ending slashes, spruced up pieces list

--- mysite/gallery/urls.py
+++ mysite/gallery/urls.py
@@ -7,26 +7,26 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     # ex: /gallery/pieces
-    path('pieces', views.pieces_list_view, name='pieces_list_view'),
+    path('pieces/', views.pieces_list_view, name='pieces_list_view'),
 
     # ex: /gallery/pieces/5
-    path('pieces/<int:piece_id>', views.piece_detail, name='piece_detail'),
+    path('pieces/<int:piece_id>/', views.piece_detail, name='piece_detail'),
 
     # ex: /gallery/pieces/5/edit
-    path('pieces/<int:piece_id>/edit', views.edit_gallery_piece, name='piece_edit'),
+    path('pieces/<int:piece_id>/edit/', views.edit_gallery_piece, name='piece_edit'),
 
     # ex: /gallery/pieces/5/edit/submit
-    path('pieces/<int:piece_id>/submitedit', views.edit_gallery_piece, name='edit_gallery_piece'),
+    path('pieces/<int:piece_id>/submitedit/', views.edit_gallery_piece, name='edit_gallery_piece'),
 
     # ex: /gallery/exhibitions
-    path('exhibitions', views.exhibitions_list_view, name='exhibitions_list_view'),
+    path('exhibitions/', views.exhibitions_list_view, name='exhibitions_list_view'),
 
     # ex: /gallery/exhib/5
-    path('exhib/<int:exhibition_id>', views.exhibition_detail, name='exhibition_detail'),
+    path('exhib/<int:exhibition_id>/', views.exhibition_detail, name='exhibition_detail'),
 
     # ex: /gallery/add-gallery-piece
-    path('add-gallery-piece', views.get_new_gallery_piece, name='get_new_gallery_piece'),
+    path('add-gallery-piece/', views.get_new_gallery_piece, name='get_new_gallery_piece'),
 
     # ex: /gallery/add-exhibition
-    path('add-exhibition', views.get_new_exhibition, name='get_new_exhibition')
+    path('add-exhibition/', views.get_new_exhibition, name='get_new_exhibition')
 ]
